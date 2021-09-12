5cbb69db8b14c73d3304390967fe938d692877f2
topzz100 <adenekantope@gmail.com>
2021-09-12 17:10:10 +0100

    working

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
+// setInterval(setTime, 1000)
 
-setTimeout = () => {
+const hourHand = document.querySelector('.hour-hand')
+const minuteHand = document.querySelector('.minute-hand')
+const secondHand = document.querySelector('.second-hand')
+
+
+setInterval(()=>{
+  setTime()
+}, 1000)
+
+ function setTime(){
   const now = new Date()
-  console.log(now.getSeconds());
-}
-setTimeout()
+
+  const seconds = now.getSeconds()/60;
+  const minutes = (seconds + now.getMinutes())/60
+  const hours = (minutes + now.getHours())/12
+
+  // addTime = () => {
+
+  // }
+
+  function addTime(element, value){
+    element.style.transform = `translate(-50%) rotate(${value*360}deg)`
+  }
+
+  addTime(hourHand, hours)
+  addTime(minuteHand, minutes)
+  addTime(secondHand, seconds)
+ 
+console.log(now.getHours())
+ }
+ setTime()
+
